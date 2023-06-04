c002b60f97ab875eb442063d69cc02fea7bb23c7
owusuyj <yawjnr@vt.edu>
2023-06-04 02:28:40 -0500

    Add key functions

--- script.js
+++ script.js
@@ -4,20 +4,49 @@ function getComputerChoice(){
     else if (ran == 1){return "PAPER"}
     else {return "SCISSORS"}
 }
+function getUserChoice(){
+    let ans = window.prompt("Chose a stance");
+    return ans;
+}
 
 console.log(getComputerChoice());
-
+console.log(getUserChoice());
 
 function playRound(playerChoice, cpuChoice)
 {
-    let playerChosen = playerChoice.toUpperCase();
-    if (cpuChoice === playerChosen){
+    playerChoice = playerChoice.toUpperCase();
+    if (cpuChoice === playerChoice){
         return "Tie! Play Again"
     }
 
-    else if (cpuChoice === "ROCK" && playerChosen == "PAPER"){
+    else if (cpuChoice === "ROCK" && playerChoice == "PAPER"){
         return "You Win! Paper beats Rock" 
+    }
+
+    else if (cpuChoice === "PAPER" && playerChoice == "ROCK"){
+        return "You Lose! Paper beats Rock"
+    }
+
+    else if (cpuChoice === "ROCK" && playerChoice == "SCISSORS"){
+        return "You Lose! Rock beats Scissors"
+    }
+
+    else if (cpuChoice === "PAPER" && playerChoice == "SCISSORS"){
+        return "You Win! Scissors beats Paper"
+    }
+
+    else if (cpuChoice === "SCISSORS" && playerChoice == "ROCK"){
+        return "You Win! Rock beats Scissors"
+    }
+    else if (cpuChoice === "SCISSORS" && playerChoice == "PAPER") {
+        return "You Lose! Scissors beats Paper"
     }
 }
 
-console.log(playRound("paper", getComputerChoice()));
+console.log(playRound(getUserChoice(), getComputerChoice()));
+
+function game(){
+    for (let i = 0; i < 5; i++){
+        playRound();
+    }
+}
