d074ede7fc5e8d709872bebaf9fda3c6d1793f64
owusuyj <yawjnr@vt.edu>
2023-06-13 17:14:04 -0500

    Added playRound

--- script.js
+++ script.js
@@ -4,36 +4,43 @@ function getComputerChoice(){
     return choices[Math.floor(Math.random() * 3)]
 }
 
-// function playRound(playerChoice, cpuChoice)
-// {
-//     playerChoice = playerChoice.toUpperCase();
-//     if (cpuChoice == playerChoice){
-//         return "Tie! Play Again"
-//     }
+function getUserChoice(){
+    buttons.forEach(button => button.addEventListener('click', function userResponse(e){
+        const key = button.className;
+        return key;
+    }));
+}
 
-//     else if (cpuChoice == "ROCK" && playerChoice == "PAPER"){
-//         return "You Win! Paper beats Rock" 
-//     }
+function playRound(playerChoice, cpuChoice)
+{
+    playerChoice = playerChoice.toUpperCase();
+    if (cpuChoice == playerChoice){
+        return "Tie! Play Again"
+    }
 
-//     else if (cpuChoice == "PAPER" && playerChoice == "ROCK"){
-//         return "You Lose! Paper beats Rock"
-//     }
+    else if (cpuChoice == "ROCK" && playerChoice == "PAPER"){
+        return "You Win! Paper beats Rock" 
+    }
 
-//     else if (cpuChoice == "ROCK" && playerChoice == "SCISSORS"){
-//         return "You Lose! Rock beats Scissors"
-//     }
+    else if (cpuChoice == "PAPER" && playerChoice == "ROCK"){
+        return "You Lose! Paper beats Rock"
+    }
 
-//     else if (cpuChoice == "PAPER" && playerChoice == "SCISSORS"){
-//         return "You Win! Scissors beats Paper"
-//     }
+    else if (cpuChoice == "ROCK" && playerChoice == "SCISSORS"){
+        return "You Lose! Rock beats Scissors"
+    }
 
-//     else if (cpuChoice == "SCISSORS" && playerChoice == "ROCK"){
-//         return "You Win! Rock beats Scissors"
-//     }
-//     else if (cpuChoice == "SCISSORS" && playerChoice == "PAPER") {
-//         return "You Lose! Scissors beats Paper"
-//     }
-// }
+    else if (cpuChoice == "PAPER" && playerChoice == "SCISSORS"){
+        return "You Win! Scissors beats Paper"
+    }
+
+    else if (cpuChoice == "SCISSORS" && playerChoice == "ROCK"){
+        return "You Win! Rock beats Scissors"
+    }
+    else if (cpuChoice == "SCISSORS" && playerChoice == "PAPER") {
+        return "You Lose! Scissors beats Paper"
+    }
+}
 
 // console.log(playRound(getUserChoice(), getComputerChoice()));
 
@@ -68,10 +75,8 @@ function playRound(){
 }
 
 
-buttons.forEach(button => button.addEventListener('click', function userResponse(e){
-    const key = button.className;
-    console.log(key)
-}));
+
+
 
 function check(e){
     return this.value  
